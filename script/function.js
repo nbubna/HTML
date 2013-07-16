5b1a1d352ac000880b9484fff2a4bac8cbeb6a36
Nathan Bubna <nathan@esha.com>
2013-07-15 22:16:56 -0700

    get demo page working

--- script/function.js
+++ script/function.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	//The intent edit
 	var timeout;
 	input.addEventListener("keydown", function() {
-		if(timeout) clearTimeout(timeout);
+		if (timeout) clearTimeout(timeout);
 		timeout = setTimeout(executeInput, 1000);
 	});
 
@@ -26,20 +26,21 @@ function executeInput() {
 	script.innerHTML = data;
 	script.id = "executable";
 
-	if(doc.getElementById("executable")) doc.body.removeChild(doc.getElementById("executable"));
+	if (doc.getElementById("executable")) {
+		doc.body.removeChild(doc.getElementById("executable"));
+	}
 	doc.body.appendChild(script);
 }
 
 function animateText(from, to, fn, done) {
 	var str = from, i = to.length, index = (to.indexOf(from) == 0) ? from.length : 0;
-	console.log(index);
+	//console.log(index);
 	(function animate() {
-		if(str.length > index) str = str.substr(0, str.length - 1), fn(str);
+		if (str.length > index) str = str.substr(0, str.length - 1), fn(str);
 		else {
-			if(i) fn(to.substr(0, (to.length - (i-1)) + index)), i--;
+			if (i) fn(to.substr(0, (to.length - (i-1)) + index)), i--;
 			else return done();
 		}
-
 		setTimeout(animate, 50);
 	})();
 }
@@ -50,40 +51,36 @@ var Storyboard = {
 	pause: function() {
 		Storyboard.running = false;
 	},
-
 	resume: function() {
 		Storyboard.running = true;
 		Storyboard.nextStory();
 	},
-
 	nextStory: function() {
-		if(Storyboard.running) {
+		if (Storyboard.running) {
 			var code = Storyboard.story[Storyboard.current];
-			if(code) {
+			if (code) {
 				animateText(input.value, code, function(str) {
 					input.value = str;
 				}, function() {
 					executeInput();
 					setTimeout(Storyboard.nextStory, 2000);
 				});
-
 				Storyboard.current++;
 			}
 		}
 	},
-
 	story: [
-		"HTML.div",
-		"HTML.div.section",
-		"HTML.div.section.eq(1)",
-		"HTML.div.section.eq(1).ul.li",
-		"HTML.div.section.eq(1).ul.li.use(function(li, i) {\n\tli.textContent = \"List item #\" + i; \n});",
+		"HTML.body.div",
+		"HTML.body.div.section",
+		"HTML.body.div.section.only(1)",
+		"HTML.body.div.section.only('#full').ul.li",
+		"HTML.body.div.section.only(-1).ul.li\n  .each(function(li, i) {\n    li.textContent = \"List item #\" + i; \n  });",
 		"HTML.find(\"#empty\")",
-		"HTML.find(\"#empty\").create.h1",
-		"HTML.find(\"#empty\").create.h1.em\n\t.textContent = \"Another h1\";",
-		"HTML.find(\"#empty\").create.h3.mult(3)",
+		"HTML.find(\"#empty\").add('h1')",
+		"HTML.find(\"#empty\").add('h1>em{Another h1}')",
+		"HTML.find(\"#empty\").add('h3*3')",
 		"HTML.find(\"#empty\").innerHTML = \"\"",
-		"HTML.find(\"#empty\").create.ul.li.mult(10)\n\t.use(function(li, i) {\n\tli.textContent = \"More list items!\"; \n});",
-		"Now you try it out for yourself! Edit me."
+		"HTML.find(\"#empty\").add('ul>li{More items!}*10')",
+		"//Now you try it out for yourself! Edit me."
 	]
 };
