65da2218aaaa9830a249e3abfb5205e6480bcb4c
Nathan Bubna <nathan@esha.com>
2013-08-23 09:24:13 -0700

    update for v0.10.0

--- script/HTML.js
+++ script/HTML.js
@@ -1,23 +1,23 @@
-/*! HTML - v0.9.3 - 2013-08-15
+/*! HTML - v0.10.0 - 2013-08-23
 * http://nbubna.github.io/HTML/
 * Copyright (c) 2013 ESHA Research; Licensed MIT, GPL */
 (function(window, document, Observer) {
     "use strict";
 
     var _ = {
-        version: "0.9.3",
+        version: "0.10.0",
         slice: Array.prototype.slice,
-        list: function(list) {
-            if (list.length === 1){ return _.node(list[0]); }
-            if (!list.each) {
+        list: function(list, force) {
+            if (list.length === 1){ return _.node(list[0], force); }
+            if (force || !list.each) {
                 if (!list.slice){ list = _.slice.call(list); }
                 _.methods(list);
-                //TODO: children traversal into first child of list
+                if (list.length){ _.children(list[0], list); }// proxy dot-traversal into first element
             }
             return list;
         },
-        node: function(node) {
-            if (!node.each) {
+        node: function(node, force) {
+            if (force || !node.each) {
                 _.methods(node);
                 _.children(node);
             }
@@ -25,56 +25,78 @@
         },
         methods: function(o) {
             for (var method in _.fn) {
-                o[method] = _.fn[method].bind(o);
+                _.define(o, method, _.fn[method]);
             }
         },
-        children: function(node) {
+        children: function(node, list) {
             var children = node._children = {};
             for (var i=0, m=node.childNodes.length; i<m; i++) {
                 var child = node.childNodes[i],
                     key = _.key(child);
                 (children[key]||(children[key]=[])).push(child);
-                if (!(key in node)){ _.define(node, key); }
+                _.define(node, key);
+                if (list){ _.define(list, key, undefined, node); }
             }
             return children;
         },
         key: function(node) {
             return node.tagName ? node.tagName.toLowerCase() : '_other';
         },
-        define: function(node, key) {
-            try {
-                Object.defineProperty(node, key, {
-                    get: function() {
-                        if (!node._children){ _.children(node); }
-                        return _.list(node._children[key]||[]);
+        define: function(o, key, val, node) {
+            if (!(key in o)) { try {// never redefine, never fail
+                node = node || o;// children needn't belong to define's target
+                Object.defineProperty(o, key,
+                    val !== undefined ? { value: val } :
+                    {
+                        get: function() {
+                            if (!node._children){ _.children(node); }
+                            return _.list(node._children[key]||[]);
+                        }
                     }
-                });
-            } catch (e) {}
+                );
+            } catch (e) {} }
         },
         mutation: function(e) {
             var node = e.target;// only wipe cache for 3rd party changes
             delete node[node._internal ? '_internal' : '_children'];
         },
+        unique: function(node, i, arr){ return arr.indexOf(node) === i; },
         fn: {
             each: function(fn) {
                 var self = this.forEach ? this : [this],
-                    fields;
+                    results = [],
+                    prop, args;
                 if (typeof fn === "string") {
-                    fields = [];
-                    fn = _.field.apply(self, arguments);
+                    prop = _.resolve[fn] || fn;// e.g. _.resolve['+class'] = 'classList.add';
+                    args = _.slice.call(arguments, 1);
+                    fn = function(el, i){ return _.resolve(prop, el, args, i); };
                 }
-                self.forEach(function(el, i, arr) {
-                    var ret = fn.call(self, _.node(el), i, arr);
-                    if (fields && ret !== undefined) {
-                        fields.push(ret);
+                for (var i=0,m=self.length, result; i<m; i++) {
+                    result = fn.call(self, _.node(self[i]), i, self);
+                    if (result || (prop && result !== undefined)) {
+                        if (result.forEach) {
+                            results.push.apply(results, result);
+                        } else {
+                            results.push(result);
+                        }
                     }
-                });
-                return fields && fields.length ? fields : this;
+                }
+                return !results[0] && results[0] !== false ? this :
+                    results[0].matches ? _.list(results.filter(_.unique)) :
+                    //self.length === 1 ? results[0] :
+                    results;
             },
-            find: function(selector) {
+            find: function() {
+                try{ window.console.warn('find() is deprecated. Please use query().'); }
+                finally{ return this.query.apply(this, arguments); }
+            },
+            query: function(selector) {
                 var self = this.forEach ? this : [this];
-                for (var list=[],i=0,m=self.length; i<m; i++) {
-                    list = list.concat(_.slice.call(self[i].querySelectorAll(selector)));
+                for (var list=[], i=0, m=self.length; i<m; i++) {
+                    var nodes = self[i].querySelectorAll(selector);
+                    for (var j=0, l=nodes.length; j<l; j++) {
+                        list.push(nodes[j]);
+                    }
                 }
                 return _.list(list);
             },
@@ -85,15 +107,10 @@
                         self.slice(b, e || (b + 1) || undefined) :
                         self.filter(
                             typeof b === "function" ? b :
-                            function(el){ return el[_.matches](b); }
+                            function(el){ return el.matches(b); }
                         )
                 );
             }
-        },
-        field: function(key) {
-            var args = _.slice.call(arguments, 1);
-            key = _.field[key] || key;// e.g. _.fn.each['+class'] = 'classList.add';
-            return function(el, i){ return _.resolve(key, el, args, i); };
         },
         resolve: function(_key, _el, args, i) {
             var key = _key, el = _el;// copy prefixed originals so we can recover them if need be
@@ -111,8 +128,10 @@
                 else if (args) { el[key] = args[0]; }
                 else { return val; }
             }
-            else if (args) { el.setAttribute(_key, args[0]); }
-            else { return el.getAttribute(_key); }
+            else if (args) {
+                if (args[0] === null){ _el.removeAttribute(_key); }
+                else { _el.setAttribute(_key, args[0]); }
+            } else { return _el.getAttribute(_key); }
         },
         fill: function(args, index, el) {
             var ret = [];
@@ -129,20 +148,21 @@
 
     var HTML = _.node(document.documentElement);// early availability
     HTML._ = _;
-    HTML.ify = function(o) {
-        return !o || 'length' in o ? _.list(o||[]) : _.node(o);
-    };
-    ['webkitM','mozM','msM','m'].forEach(function(prefix) {
-        if (HTML[prefix+'atchesSelector']) {
-            _.matches = prefix+'atchesSelector';
-        }
+    _.define(HTML, 'ify', function(o, force) {
+        return !o || 'length' in o ? _.list(o||[], force) : _.node(o, force);
     });
+    // ensure element.matches(selector) availability
+    var Ep = Element.prototype,
+        aS = 'atchesSelector';
+    _.define(Ep, 'matches', Ep['m'] || Ep['webkitM'+aS] || Ep['mozM'+aS] || Ep['msM'+aS]);
+    // watch for changes in children
     if (Observer) {
         new Observer(function(list){ list.forEach(_.mutation); })
             .observe(HTML, { childList: true, subtree: true });
     } else {
         document.addEventListener("DOMSubtreeModified", _.mutation);
     }
+    // export 
     if (typeof define === 'function' && define.amd) {
         define(function(){ return HTML; });
     } else if (typeof module !== 'undefined' && module.exports) {
@@ -151,7 +171,7 @@
         window.HTML = HTML;
     }
     // eventual consistency
-    document.addEventListener("DOMContentLoaded", function(){ _.children(HTML); });
+    document.addEventListener("DOMContentLoaded", function(){ _.node(HTML, true); });
 
 })(window, document, window.MutationObserver);
 
@@ -159,11 +179,9 @@
     "use strict";
 
     var add = _.fn.add = function(arg, ref) {
-        var list = [];
-        this.each(function(node) {
-            list = list.concat(add.all(node, arg, ref));
+        return this.each(function(node) {
+            return add.all(node, arg, ref);
         });
-        return _.list(list);
     };
     add.all = function(node, arg, ref) {
         if (typeof arg === "string") {// turn arg into an appendable
@@ -206,20 +224,114 @@
         _.children(node);
     };
 
-    _.fn.remove = function(noDeadEnd) {
-        var parents = [];
-        this.each(function(node) {
+    _.fn.remove = function(chain) {
+        return this.each(function(node) {
             var parent = node.parentNode;
-            if (noDeadEnd && parents.indexOf(parent) < 0) {
-                parents.push(parent);
+            if (parent) {
+                parent.removeChild(node);
+                _.updated(parent);
+                if (chain){ return parent; }
             }
-            parent.removeChild(node);
-            _.updated(parent);
         });
-        return noDeadEnd ? _.list(parents) : this;
     };
 
 })(document, document.documentElement._);
+
+(function(document, HTML) {
+    "use strict";
+
+    var _ = HTML._,
+    event = _.fn.event = function() {
+        try{ window.console.warn('event() is deprecated. https://github.com/nbubna/HTML/issues/1'); } catch(e){}
+        var args = _.slice.call(arguments),
+            self = this,
+            action,
+            ret = [];
+        if (!args[0]) {// first is falsy
+            action = 'off';
+            args.shift();
+            if (typeof args[1] === "function"){ args.splice(1, 0, false); }// selector omitted
+        } else if (!args[1] || args[1].forEach) {// second is absent or data
+            action = 'trigger';
+        } else {
+            action = 'on';
+            if (args[0] === 1){ args[4] = args.shift(); }// first is _once
+            if (!args[2] || args[2].forEach) {// selector omitted
+                args.splice(1, 0, false);
+                args.splice(4, 1);// put _once back as fifth arg
+            }
+            args[3] = (args[3]||[]).slice(0);// don't let listener data change
+            if (typeof args[2] === "string") {// third is _prop for each
+                args[5] = args[2];
+                args[3].unshift(args[2]);// _prop becomes first item in data
+                args[2] = _.fn.each;// and is replace by each()
+            }
+        }
+        (args[0]||'').split(' ').forEach(function(type) {
+            args[0] = type;
+            self.each(function(node) {
+                ret.push(event[action].apply(node, args));
+            });
+        });
+        return action === 'trigger' ? ret.length === 1 ? ret[0] : ret : this;
+    },
+    concat = Array.prototype.concat;
+
+    event.on = function(type, selector, fn, data, _once, _prop) {
+        var listener = function(e) {
+            event.heard.call(this, e, selector, fn, data, _once, _prop);
+        };
+        this.addEventListener(type, listener);
+        _.define(this, '_evt', []);
+        this._evt.push([type, selector, _prop||fn, listener]);
+    };
+    event.off = function(type, selector, fn) {
+        if (this._evt) {
+            for (var i=0; i<this._evt.length; i++) {
+                var evt = this._evt[i];
+                if ((!type || evt[0] === type) &&
+                    (!selector || evt[1] === selector) &&
+                    (!fn || evt[2] === fn)) {
+                    this.removeEventListener(evt[0], evt[3]);
+                    this._evt.splice(i--, 1);
+                }
+            }
+        } else {
+            this.removeEventListener(type, fn);
+        }
+    };
+    event.trigger = function(type, data) {
+        var e = document.createEvent('HTMLEvents');
+        e.initEvent(type, true, true);
+        if (data){ e.data = data; }
+        this.dispatchEvent(e);
+        return e;
+    };
+
+    event.heard = function(e, selector, fn, data, _once, _prop) {
+        var self = selector ? event.closest.call(e, selector) : this;
+        if (self) {
+            var args = [e];
+            if (e.data){ args.push(e.data); }
+            if (data){ args.unshift(data); }
+            _.node(e.target);
+            _.define(e, 'closest', event.closest);
+            if (_once){ event.off.call(this, e.type, selector, _prop || fn); }
+            fn.apply(self, concat.apply([], args));
+        }
+    };
+    event.closest = function(selector) {
+        var el = this.target;
+        while (el && el.matches) {
+            if (el.matches(selector)) {
+                return _.node(el);
+            }
+            el = el.parentNode;
+        }
+    };
+
+})(document, document.documentElement);
+
 (function(document, _) {
     "use strict";
 
--- script/index.js
+++ script/index.js
@@ -1,19 +1,19 @@
 (function(document, Sintax, Demo, HTML) {
 	document.addEventListener("DOMContentLoaded", function() {
 		
-		new Demo(HTML.find('#input'),
-				 HTML.find('#output iframe'), [
+		new Demo(HTML.query('#input'),
+				 HTML.query('#output iframe'), [
 			"HTML.body.div",
 			"HTML.body.div.section",
 			"HTML.body.div.section.only(0)",
 			"HTML.body.div.section.only('#full').ul.li",
 			"HTML.body.div.section.only('#full').ul.li\n  .each('id', 'item${i}')",
-			"HTML.find('#empty')",
-			"HTML.find('#empty').add('h1')",
-			"HTML.find('#empty').add('ul>li{item}*5')",
-			"HTML.find('#empty li').each(function(el, i, all) {\n  el.textContent += ' '+(i+1)+' of '+all.length;\n})",
-			"HTML.find('#empty li').only(function(el, i) {\n  return i % 2;\n}).each('className','odd')",
-			"HTML.find('#empty *').remove()",
+			"HTML.query('#empty')",
+			"HTML.query('#empty').add('h1')",
+			"HTML.query('#empty').add('ul>li{item}*5')",
+			"HTML.query('#empty li').each(function(el, i, all) {\n  el.textContent += ' '+(i+1)+' of '+all.length;\n})",
+			"HTML.query('#empty li').only(function(el, i) {\n  return i % 2;\n}).each('className','odd')",
+			"HTML.query('#empty *').remove()",
 			"//Now you try it out for yourself! Edit me."
 		]);
 
