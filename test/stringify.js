534a28f6b8657534b44b5042daf3e0a1ac4a4dc0
nbubna <nathan@esha.com>
2014-10-22 09:39:33 -0700

    stub file for stringify tests

---
+++ test/stringify.js
@@ -0,0 +1,30 @@
+(function(HTML) {
+  /*
+    ======== A Handy Little QUnit Reference ========
+    http://api.qunitjs.com/
+
+    Test methods:
+      module(name, {[setup][ ,teardown]})
+      test(name, callback)
+      expect(numberOfAssertions)
+      stop(increment)
+      start(decrement)
+    Test assertions:
+      ok(value, [message])
+      equal(actual, expected, [message])
+      notEqual(actual, expected, [message])
+      deepEqual(actual, expected, [message])
+      notDeepEqual(actual, expected, [message])
+      strictEqual(actual, expected, [message])
+      notStrictEqual(actual, expected, [message])
+      throws(block, [expected], [message])
+  */
+
+    module("stringify");
+
+    test('API', function() {
+      ok(HTML._.fn.stringify);
+    });
+    
+
+}(HTML));
